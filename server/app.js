570369a16181b49c5ae1d42a5bf77612b156f69b
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-14 12:05:51 -0500

    added Mongoose for connection to Mongodb and first model

--- server/app.js
+++ server/app.js
@@ -1,26 +1,40 @@
-const express = require('express')
-require('dotenv').config()
-PORT = process.env.PORT
-const comments = require('./routes/comments')
-const posts = require('./routes/posts')
-const users = require('./routes/users')
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT = process.env.PORT;
+const URI = process.env.MONGO_URI;
+const comments = require("./routes/comments");
+const posts = require("./routes/posts");
+const users = require("./routes/users");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: `You've successfully connected!` });
+});
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: `You've successfully connected!`})
-})
+app.use("/comments", comments);
+app.use("/posts", posts);
+app.use("/users", users);
 
-app.use('/comments', comments)
-app.use('/posts', posts)
-app.use('/users', users)
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("successfully connected!");
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-})
+app.use((req, res, next, err) => {
+  console.json(err);
+  next();
+});
